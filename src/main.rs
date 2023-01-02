481ff41a53b3422c2ea2f2389303c5d595d40977
Stephen P Connolly <stephen@pedanticism.net>
2023-01-02 22:22:44 +0000

    main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+use std::env;
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
+
+    if let Ok(lines) = read_lines(file_path) {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            if let Ok(x) = line {
+
+                println!("data: {x}");
+            }
+        }
+    } else {
+        println!("Unable to open file");
+    }
 }
