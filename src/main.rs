e7c29429deedf2cc0d552aa814c27b6d4c0f7da3
Stephen P Connolly <stephen@pedanticism.net>
2023-07-27 23:22:06 +0100

    Tidied up and renamed structs

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 use std::env;
+use std::cell::RefCell;
+use std::rc::Rc;
+use std::rc::Weak;
+
 
 // The output is wrapped in a Result to allow matching on errors
 // Returns an Iterator to the Reader of the lines of the file.
@@ -26,5 +30,74 @@ fn main() {
         }
     } else {
         println!("Unable to open file");
+    }
+}
+
+
+pub struct StyledTree {
+    pub root: RefCell<Rc<StyledNode>>,
+}
+pub struct StyledNode {
+//    pub node: Node,
+    pub children: RefCell<Vec<Rc<StyledNode>>>,
+    parent: RefCell<Weak<StyledNode>>
+    
+}
+
+impl StyledTree {
+    pub fn new() -> Self {
+        StyledTree {
+            root: RefCell::new(Rc::new(StyledNode {
+//                node: Node { node_type: NodeType::Comment(String::from("comment")), children: vec![] },
+                children: RefCell::new(vec![]),
+                parent: RefCell::new(Default::default()),
+                //specified_values: Default::default()
+            }))
+        }
+    }
+    pub fn make(&self) -> Rc<StyledNode> {
+        Rc::new(StyledNode{
+/*             node: Node {
+                node_type: NodeType::Comment(String::from("comment")),
+                Children: vec![] 
+            }, */
+            children: RefCell::new(vec![]),
+            parent: RefCell::new(Weak::new()),
+            //specified_values: Default::default()
+        })
+    }
+    pub fn make_with(&self, 
+              children:RefCell<Vec<Rc<StyledNode>>>) -> Rc<StyledNode> {
+        let rc =  Rc::new(StyledNode {
+            children,
+            parent: RefCell::new(Default::default()),
+        });
+        for ch in rc.children.borrow().iter() {
+            *ch.parent.borrow_mut() = Rc::downgrade(&rc);
+        }
+        return rc;
+    }
+    pub fn set_root(&self, node:&Rc<StyledNode>) {
+        *self.root.borrow_mut() = Rc::clone(node);
+    }
+    pub fn append(&self, parent:&Rc<StyledNode>, child:&Rc<StyledNode>) {
+        parent.children.borrow_mut().push(Rc::clone(child));
+        *child.parent.borrow_mut() = Rc::downgrade(parent);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn t1() {
+        let tree = StyledTree::new();
+        let root = tree.make();
+        let child1 = tree.make();
+        let child2 = tree.make();
+        tree.set_root(&root);
+        tree.append(&root,&child1);
+        tree.append(&root,&child2);
+        assert_eq!(5, 5);
     }
 }
